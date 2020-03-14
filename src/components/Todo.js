3cf2741b0909a01b3c5b8a09402c6a72e091e48b
Jordan <jordanshehane@gmail.com>
2020-03-13 21:16:47 -0600

    finishing touches and notes

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,10 +1,13 @@
 import React from "react";
 
 const Item = props => {
+
   return (
     <div
+    /*passing in a className with an if statement to determine if the item has been completed*/
       className={`item${props.item.complete ? " complete" : ""}`}
-      onClick={e => props.toggleCompleted(props.item.id)}
+      /* passing n onclick event evaluating the toggleComplete function on it passing item.id as its argument*/
+      onClick={event => props.toggleCompleted(props.item.id)}
     >
       <p>{props.item.name}</p>
     </div>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,21 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
 import React from "react";
-
 import Todo from "./Todo";
 
 const TodoList = props => {
   return (
     <div className="shopping-list">
+
       {props.todoList.map(item => (
         <Todo
+
           key={item.id}
           item={item}
           toggleCompleted={props.toggleCompleted}
         />
       ))}
+      
       <button className="clear-btn" onClick={props.clearCompleted}>
         Clear Completed Items
       </button>
